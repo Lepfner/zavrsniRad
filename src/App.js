918edd8162a06b8628a9fae1ba9dadeae90f7b69
lepfner <lerner.andi@gmail.com>
2023-03-27 17:05:35 +0200

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import '../src/Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='text-black'>AAA</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
